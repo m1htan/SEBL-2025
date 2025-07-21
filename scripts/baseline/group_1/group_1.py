a605aad59247bd5e57633d4fde19986e8d035188
Minh Tan <ngminhtan0410@gmail.com>
2025-07-21 19:28:24 +0700

    Add Group 1 baseline data and refactor scripts
    Added data/baseline/Group_1.csv and scripts/baseline/group_1/group_1.py. Removed MinMax and ZScore scripts and related output images to consolidate baseline processing for Group 1.

---
+++ scripts/baseline/group_1/group_1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+
+csv_dir = "/Users/minhtan/Documents/GitHub/SEBL-2025/data/data_clean/volume_A"
+
+OUTPUT_DIR = "/Users/minhtan/Documents/GitHub/SEBL-2025/data/baseline"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+output_path = os.path.join(OUTPUT_DIR, f"Group_1.csv")
+
+# Danh sách tên file
+csv_files = [f"Q{i}_(b).csv" for i in range(1, 9)]
+
+# Danh sách DataFrame
+dfs = []
+
+for file_name in csv_files:
+    path = os.path.join(csv_dir, file_name)
+    try:
+        df = pd.read_csv(path)
+
+        # Đảm bảo cột đầu tiên là chuỗi
+        df.iloc[:, 0] = df.iloc[:, 0].astype(str)
+
+        # Loại bỏ hàng rỗng hoặc chỉ toàn NaN
+        df.dropna(how="all", inplace=True)
+
+        # Ép các cột còn lại về float
+        for col in df.columns[1:]:
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+
+        dfs.append(df)
+
+    except Exception as e:
+        print(f"[ERROR] Lỗi khi đọc {file_name}: {e}")
+
+# Gộp các DataFrame lại
+merged_df = pd.concat(dfs, ignore_index=True)
+
+
+print(merged_df)
+merged_df.to_csv(output_path, index=False, encoding="utf-8-sig")
+print(f"[PROCESS] Đã lưu dataframe thành file: {output_path}")
